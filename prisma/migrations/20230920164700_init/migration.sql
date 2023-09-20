1a6b165c6cb35fd4e02da776f4579433d5df70e4
Celeo <mattboulanger@fastmail.com>
2023-09-20 09:47:45 -0700

    DB init

---
+++ prisma/migrations/20230920164700_init/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "TeacherRating" (
+    "cid" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "minorGround" BOOLEAN NOT NULL DEFAULT false,
+    "majorGround" BOOLEAN NOT NULL DEFAULT false,
+    "minorTower" BOOLEAN NOT NULL DEFAULT false,
+    "majorTower" BOOLEAN NOT NULL DEFAULT false,
+    "minorApproach" BOOLEAN NOT NULL DEFAULT false,
+    "majorApproach" BOOLEAN NOT NULL DEFAULT false,
+    "center" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "TrainingSchedule" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "Instructor" INTEGER NOT NULL,
+    "dayOfWeek" INTEGER NOT NULL,
+    "timeOfDay" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "TrainingSession" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "instructor" INTEGER NOT NULL,
+    "student" INTEGER,
+    "start" TEXT NOT NULL,
+    "end" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'open',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
