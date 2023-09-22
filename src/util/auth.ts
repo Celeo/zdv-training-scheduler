077e99d7927dc3f42435c864fda0434303e63d3b
Celeo <mattboulanger@fastmail.com>
2023-09-22 13:06:10 -0700

    WIP

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,15 +1,23 @@
 import axios from "axios";
 import { nanoid } from "nanoid";
-import liboauth from "oauth";
+import liboauth, { OAuth2 } from "oauth";
 import * as jose from "jose";
 import { DB } from "../data/db";
+import { loadConfig, type Config } from "./config";
 
+/**
+ * Token data back from ZDV SSO.
+ */
 export type ZdvAccessData = {
   accessToken: string;
   refreshToken: string;
 };
 
-// this is a subset of the available information
+/**
+ * Information about a ZDV member.
+ *
+ * This is only a subset of what ZDV SSO returns.
+ */
 export type ZdvUserInfo = {
   cid: number;
   email: string;
@@ -19,6 +27,9 @@ export type ZdvUserInfo = {
   roles: Array<string>;
 };
 
+/**
+ * The payload in the JWT stored on user's browsers.
+ */
 export type JwtPayload = {
   access_token: string;
   info: ZdvUserInfo;
@@ -43,23 +54,29 @@ const GATE_TO_ROLES = {
   [RequiredPermission.Admin]: ADMIN_ROLES,
 };
 
-const ZDV_OAUTH = new liboauth.OAuth2(
-  import.meta.env.ZDV_OAUTH_CLIENT_ID,
-  import.meta.env.ZDV_OAUTH_CLIENT_SECRET,
-  import.meta.env.ZDV_OAUTH_BASE_URL,
-  import.meta.env.ZDV_OAUTH_AUTHORIZE_ENDPOINT,
-  import.meta.env.ZDV_OAUTH_TOKEN_ENDPOINT,
-);
+function getOAuth(config: Config): OAuth2 {
+  return new liboauth.OAuth2(
+    config.oauth.clientId,
+    config.oauth.clientSecret,
+    config.oauth.baseUrl,
+    config.oauth.authorizeEndpoint,
+    config.oauth.tokenEndpoint,
+  );
+}
 
 /**
  * Return the ZDV OAuth authorization URL using a random state,
  * and that state for verification.
  */
-export function getAuthorizationUrl(): { url: string; state: string } {
+export async function getAuthorizationUrl(): Promise<{
+  url: string;
+  state: string;
+}> {
   const state = nanoid();
-  const url = ZDV_OAUTH.getAuthorizeUrl({
-    redirect_uri: import.meta.env.ZDV_OAUTH_REDIRECT_URI,
-    scope: import.meta.env.ZDV_OAUTH_SCOPES,
+  const config = await loadConfig();
+  const url = getOAuth(config).getAuthorizeUrl({
+    redirect_uri: config.oauth.redirectUri,
+    scope: config.oauth.scopes,
     state,
     response_type: "code",
   });
@@ -70,12 +87,14 @@ export function getAuthorizationUrl(): { url: string; state: string } {
  * Exchange a code from ZDV OAuth for access and refresh tokens.
  */
 export async function getAccessToken(code: string): Promise<ZdvAccessData> {
+  const config = await loadConfig();
+  const oauth = getOAuth(config);
   return new Promise((resolve, reject) => {
     try {
-      ZDV_OAUTH.getOAuthAccessToken(
+      oauth.getOAuthAccessToken(
         code,
         {
-          redirect_uri: import.meta.env.ZDV_OAUTH_REDIRECT_URI,
+          redirect_uri: config.oauth.redirectUri,
           grant_type: "authorization_code",
         },
         (_: unknown, accessToken, refreshToken) => {
@@ -97,8 +116,10 @@ export async function getUserInfo(
 ): Promise<ZdvUserInfo | null> {
   // make the authenticated call back to ZDV SSO
   const { data } = await axios.get<any>(
-    import.meta.env.ZDV_OAUTH_USER_INFO_URI,
-    { headers: { Authorization: `Bearer ${access_token}` } },
+    (await loadConfig()).oauth.userInfoUri,
+    {
+      headers: { Authorization: `Bearer ${access_token}` },
+    },
   );
 
   // prevent logins if needed
@@ -112,14 +133,14 @@ export async function getUserInfo(
     return null;
   }
 
-  // payload to be put into the JWT
+  // payload to be put into the JWT; only part of the available data
   return {
     cid: data.user.cid,
     email: data.user.email,
     first_name: data.user.firstname,
     last_name: data.user.lastname,
     oi: data.user.oi,
-    roles: data.user.roles.map((role: Record<string, unknown>) => role.name),
+    roles: data.user.roles.map((role: { name: string }) => role.name),
   };
 }
 
@@ -134,7 +155,7 @@ export async function createJwt(
   access_token: string,
   info: ZdvUserInfo,
 ): Promise<string> {
-  const secret = new TextEncoder().encode(import.meta.env.JWT_SECRET);
+  const secret = new TextEncoder().encode((await loadConfig()).jwtSecret);
   return await new jose.SignJWT({ access_token, info })
     .setProtectedHeader({ alg: "HS256" })
     .setIssuer("urn:zdv:training-scheduler")
@@ -147,7 +168,7 @@ export async function createJwt(
  * Verify a JWT and return its payload.
  */
 async function verifyJwt(jwt: string): Promise<JwtPayload> {
-  const secret = new TextEncoder().encode(import.meta.env.JWT_SECRET);
+  const secret = new TextEncoder().encode((await loadConfig()).jwtSecret);
   const { payload } = await jose.jwtVerify(jwt, secret, {
     issuer: "urn:zdv:training-scheduler",
     audience: "urn:zdv:training-scheduler",
@@ -159,6 +180,10 @@ async function verifyJwt(jwt: string): Promise<JwtPayload> {
  * Check the authorization header from the request, retrieving and
  * validating the JWT, and ensuring the user has the ability to
  * access logged-in-only parts of the site.
+ *
+ * If there is an error with the user's authentication, a `Response`
+ * will be returned from this function, which should be returned
+ * to the user directly.
  */
 export async function checkAuth(
   request: Request,
---
+++ src/util/config.ts
@@ -0,0 +1,35 @@
+import * as toml from "toml";
+import memoize from "memoizee";
+import { promises } from "fs";
+const { readFile } = promises;
+
+/**
+ * App configuration.
+ */
+export type Config = {
+  /** Signing secret for JWTs */
+  jwtSecret: string;
+  /** ZDV SSO data */
+  oauth: {
+    clientId: string;
+    clientSecret: string;
+    baseUrl: string;
+    authorizeEndpoint: string;
+    tokenEndpoint: string;
+    redirectUri: string;
+    scopes: string;
+    userInfoUri: string;
+  };
+};
+
+async function _loadConfig(): Promise<Config> {
+  const text = await readFile(".config.toml", { encoding: "utf-8" });
+  return toml.parse(text);
+}
+
+/**
+ * Retrieve and return the app configuration from disk.
+ *
+ * This function is memoized.
+ */
+export const loadConfig = memoize(_loadConfig);
