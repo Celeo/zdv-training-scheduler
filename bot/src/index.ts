b08061499c9903d575bd65d39dd3c86335382a79
Celeo <mattboulanger@fastmail.com>
2023-10-12 11:35:26 -0700

    Session Discord reminders
    Untested

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,17 +1,27 @@
 import { Client, Events } from "discord.js";
 import { Cron } from "croner";
+import { DateTime } from "luxon";
 
 const SITE_URL_ENV_VAR = "SITE_URL";
 const SITE_TOKEN_ENV_VAR = "SITE_TOKEN";
 const BOT_TOKEN_ENV_VAR = "DISCORD_BOT_TOKEN";
 
 /**
- * Data from the site GET request.
+ * Data from the site messages request.
  */
-type SiteResponse = Array<{
+type MessagesResponse = Array<{
   id: number;
   cid: number;
   message: string;
+}>;
+
+/**
+ * Data from the site pending request.
+ */
+type PendingSessionsResponse = Array<{
+  student?: number;
+  date: string;
+  time: string;
 }>;
 
 /**
@@ -64,7 +74,7 @@ async function checkForMessages(): Promise<void> {
       );
       return;
     }
-    const data: SiteResponse = await resp.json();
+    const data: MessagesResponse = await resp.json();
     for (const message of data) {
       const discordUser = await getDiscordUserIdFromCid(message.cid);
       if (discordUser === undefined) {
@@ -87,18 +97,55 @@ async function checkForMessages(): Promise<void> {
       console.log(`Sent message for #${message.id}`);
     }
   } catch (err) {
-    console.error(`An error occurred: ${err}`);
+    console.error(`Error getting messages: ${err}`);
   }
 }
 
-// check pending messages and deliver
+// check pending messages and deliver, every minute
 Cron("* * * * *", async () => {
   await checkForMessages();
 });
 
-// session reminders, 1 hour before
+/**
+ * Pull upcoming sessions from the site and send reminders
+ * if they start in the next hour (and the user has Discord
+ * notifications enabled).
+ */
+async function checkForReminders(): Promise<void> {
+  try {
+    const siteUrl = process.env[SITE_URL_ENV_VAR];
+    let resp = await fetch(`${siteUrl}api/sessions/mine?pending=true`);
+    const pendingSessions: PendingSessionsResponse = await resp.json();
+    const now = DateTime.utc();
+    for (const session of pendingSessions) {
+      const recordDate = DateTime.fromISO(`${session.date}T${session.time}`, {
+        zone: "utc",
+      });
+      if (now < recordDate) {
+        const delta = now.toMillis() - recordDate.toMillis();
+        if (delta > 0 && delta <= 1_000 * 60 * 60) {
+          const discordUser = await getDiscordUserIdFromCid(session.student!);
+          if (discordUser === undefined) {
+            console.error(
+              `Could not send Discord message to cid ${session.student} - could not find Discord user id`
+            );
+            continue;
+          }
+          await client.users.send(
+            discordUser,
+            `Your training session starts in 1 hour.`
+          );
+        }
+      }
+    }
+  } catch (err) {
+    console.error(`Error getting upcoming sessions: ${err}`);
+  }
+}
+
+// session reminders, every hour
 Cron("0 * * * *", async () => {
-  // TODO
+  await checkForReminders();
 });
 
 /*
