7c9e52727f6245b93056f206f8838759bdb2a4db
Celeo <mattboulanger@fastmail.com>
2023-10-16 15:27:29 -0700

    Fix Prisma types not playing nice with Vite

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,11 +1,20 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 import node from "@astrojs/node";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: node({ mode: "standalone" })
+  adapter: node({ mode: "standalone" }),
+
+  // https://github.com/prisma/prisma/issues/12504#issuecomment-1285883083
+  vite: {
+    resolve: {
+      alias: {
+        ".prisma/client/index-browser":
+          "./node_modules/.prisma/client/index-browser.js",
+      },
+    },
+  },
 });
