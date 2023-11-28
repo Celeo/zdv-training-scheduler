3873b0a1072fa6cf027f8747d2ad7c29401b6f5f
Celeo <mattboulanger@fastmail.com>
2023-11-27 21:10:31 -0800

    Put alerts above modal shading

--- site/src/components/Alerts.tsx
+++ site/src/components/Alerts.tsx
@@ -28,7 +28,7 @@ export function Alerts() {
   };
 
   return (
-    <div className="absolute top-5 right-5">
+    <div className="absolute top-5 right-5 z-50">
       {localAlerts.map(({ level, message, uuid }) => {
         let icon;
         if (level === "INFO") {
