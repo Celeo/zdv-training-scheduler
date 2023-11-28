02a8747d85fd734e2c63a27e4b4b6db09c4dc275
Celeo <mattboulanger@fastmail.com>
2023-11-27 22:05:26 -0800

    WIP

--- site/src/pages/api/sessions/index.ts
+++ site/src/pages/api/sessions/index.ts
@@ -41,7 +41,7 @@ export async function GET(
   const dayEnd = dayStart.endOf("day");
 
   // find the sessions on the date and the schedules on the day of the week
-  const sessions = await DB.trainingSession.findMany({
+  const sessionsOnDate = await DB.trainingSession.findMany({
     where: {
       dateTime: {
         lte: dayEnd.toUTC().toJSDate(),
@@ -50,91 +50,103 @@ export async function GET(
     },
   });
 
-  const schedules = (
-    await DB.trainingSchedule.findMany({
-      include: { trainingScheduleExceptions: true },
-    })
-  ).filter((s) => {
-    let d = DateTime.fromISO(
-      `${DateTime.utc().minus({ day: 1 }).toISODate()}T${s.timeOfDay}`,
-      { zone: "utc" },
-    );
-    while (d.weekday !== s.dayOfWeek) {
-      d = d.plus({ day: 1 });
-    }
-    // allow taking sessions out to a number of weeks
-    for (let week = 0; week < SCHEDULE_WEEK_OUTLOOK; week++) {
-      const d2 = d.plus({ week });
-      if (dayStart <= d2 && d2 <= dayEnd) {
-        return true;
-      }
-    }
-    return false;
+  const nowUtc = DateTime.utc().toJSDate();
+  const schedules = await DB.trainingSchedule.findMany({
+    include: { trainingScheduleExceptions: true },
   });
 
-  // for those schedules, filter down to those that don't have a session on this date
-  const schedulesWithoutSessions = schedules.filter(
-    (schedule) =>
-      sessions.find((session) => session.scheduleId === schedule.id) ===
-      undefined,
-  );
-
-  // for schedules that should "tick" on this date, filter to those that
-  // the owner hasn't expressly cancelled (deleted), and add those to
-  // the list of sessions to show to the user (with a mock id of -1)
-  const nowUtc = DateTime.utc().toJSDate();
-  schedulesWithoutSessions
+  schedules
+    .filter((s) => {
+      // filter to schedules that would tick on the date
+      let d = DateTime.fromISO(
+        `${DateTime.utc().minus({ day: 1 }).toISODate()}T${s.timeOfDay}`,
+        { zone: "utc" },
+      );
+      while (d.weekday !== s.dayOfWeek) {
+        d = d.plus({ day: 1 });
+      }
+      // only allow taking sessions out to a certain number of weeks
+      for (let week = 0; week < SCHEDULE_WEEK_OUTLOOK; week++) {
+        const d2 = d.plus({ week });
+        if (dayStart <= d2 && d2 <= dayEnd) {
+          return true;
+        }
+      }
+      return false;
+    })
     .filter(
+      // filter out those that have a session on this date already
+      (schedule) =>
+        sessionsOnDate.find((session) => session.scheduleId === schedule.id) ===
+        undefined,
+    )
+    .filter(
+      // filter out exclusions
       (schedule) =>
         !schedule.trainingScheduleExceptions.some(
           (except) => except.date === dateStr,
         ),
     )
-    .map((schedule) => ({
-      id: -1,
-      scheduleId: schedule.id,
-      trainer: schedule.trainer,
-      student: null,
-      position: null,
-      dateTime: DateTime.fromISO(`${dateStr}T${schedule.timeOfDay}`, {
-        zone: "utc",
-      }).toJSDate(),
-      status: SESSION_STATUS.OPEN,
-      notes: "",
-      createdAt: nowUtc,
-      updatedAt: nowUtc,
-    }))
-    .forEach((s) => sessions.push(s));
+    .filter(
+      // filter out stuff that'd be in the past
+      (schedule) =>
+        // FIXME dateStr is local timezone
+        DateTime.fromISO(`${dateStr}T${schedule.timeOfDay}`, {
+          zone: "utc",
+        }) > DateTime.utc(),
+    )
+    .map(
+      // create the session
+      (schedule) => ({
+        id: -1,
+        scheduleId: schedule.id,
+        trainer: schedule.trainer,
+        student: null,
+        position: null,
+        // FIXME dateStr is local timezone
+        dateTime: DateTime.fromISO(`${dateStr}T${schedule.timeOfDay}`, {
+          zone: "utc",
+        }).toJSDate(),
+        status: SESSION_STATUS.OPEN,
+        notes: "",
+        createdAt: nowUtc,
+        updatedAt: nowUtc,
+      }),
+    )
+    .forEach(
+      // append to the sessions list
+      (s) => sessionsOnDate.push(s),
+    );
 
-  const ret: Array<TrainingSession> = [];
+  const returnSessions: Array<TrainingSession> = [];
   if (canBeTrainer(auth.data.info)) {
     // For trainers, their response includes all open sessions (as even trainers need
     // training sometimes), but also include their sessions on the date in case they
     // need to cancel them.
-    sessions
+    sessionsOnDate
       .filter(
         (session) =>
           session.status === SESSION_STATUS.OPEN ||
           session.trainer === auth.data.info.cid,
       )
-      .forEach((session) => ret.push(session));
+      .forEach((session) => returnSessions.push(session));
   } else {
     // Now filter to sessions that are open. This cannot have been done as part of the DB
     // query, since we needed to know _all_ the sessions on the date to determine
     // which schedules should "tick".
-    sessions
+    sessionsOnDate
       .filter((session) => session.status === SESSION_STATUS.OPEN)
-      .forEach((session) => ret.push(session));
+      .forEach((session) => returnSessions.push(session));
   }
 
   // sort by time of day for better UX
-  ret.sort(
+  returnSessions.sort(
     (a, b) =>
       DateTime.fromJSDate(a.dateTime).hour -
       DateTime.fromJSDate(b.dateTime).hour,
   );
 
-  return new Response(JSON.stringify(ret));
+  return new Response(JSON.stringify(returnSessions));
 }
 
 type UpdatePayload = {
